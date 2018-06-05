2afd0d96c8e7edfaef2e767276c3ecf152642569
Sebastian Ferrari <sebastianferrari@live.com>
2018-06-05 04:25:12 +0000

    all is working fine

--- src/App.js
+++ src/App.js
@@ -2,15 +2,79 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
+// const value1 = Math.floor(Math.random() * 100);
+// const value2 = Math.floor(Math.random() * 100);
+// const value3 = Math.floor(Math.random() * 100);
+// const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
+// const numQuestions = 0;
+// const numCorrect = 0;
 
 class App extends Component {
-  render() {
+  state = {
+    value1: 0,
+    value2: 0,
+    value3: 0,
+    proposedAnswer: 0,
+    numQuestions: 0,
+    numCorrect: 0
+  }
+
+  refresh = () => {
+    let v1 = Math.floor(Math.random() * 100);
+    let v2 = Math.floor(Math.random() * 100);
+    let v3 = Math.floor(Math.random() * 100);
+    let pa = Math.floor(Math.random() * 3) + v1 + v2 + v3;
+
+    this.setState({
+      value1: v1,
+      value2: v2,
+      value3: v3,
+      proposedAnswer: pa
+    })
+  }
+
+  increaseQuestionNumber = () => {
+    this.setState((prevState, props) => ({
+      numQuestions: prevState.numQuestions + 1
+    }));
+  }
+
+  increaseCorrectAnswers = () => {
+    this.setState((prevState, props) => ({
+      numCorrect: prevState.numCorrect + 1
+    }))
+  }
+
+  getCorrectAnswer = () => {
+    return this.state.value1 + this.state.value2 + this.state.value3;
+  }
+
+  truePressed = () => {
+    // console.log("TRUE");
+    var correctAnswer = this.getCorrectAnswer();
+    if (correctAnswer === this.state.proposedAnswer) {
+      this.increaseCorrectAnswers();
+    }
+    
+    this.increaseQuestionNumber();
+    this.refresh();
+  }
+
+  falsePressed = () => {
+    var correctAnswer = this.getCorrectAnswer();
+    if (correctAnswer !== this.state.proposedAnswer) {
+      this.increaseCorrectAnswers();
+    }
+    
+    this.increaseQuestionNumber();
+    this.refresh();
+  }
+
+  componentDidMount() {
+    this.refresh();
+  }
+  
+  render() {    
     return (
       <div className="App">
         <header className="App-header">
@@ -20,12 +84,12 @@ class App extends Component {
         <div className="game">
           <h2>Mental Math</h2>
           <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+            <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${this.state.proposedAnswer}`}</p>
           </div>
-          <button>True</button>
-          <button>False</button>
+          <button onClick={this.truePressed}>True</button>
+          <button onClick={this.falsePressed}>False</button>
           <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
+            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
           </p>
         </div>
       </div>
